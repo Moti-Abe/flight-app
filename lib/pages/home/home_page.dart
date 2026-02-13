4c0d3be339deeed27f3fe2b8b0a3e5ecf5c9576a
Tuamay <tuamayassefa@gmail.com>
2026-02-13 02:59:33 -0800

    updated home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,11 +1,11 @@
-import 'package:expedia/pages/home/auth/sign_in_page.dart';
+// import 'package:expedia/pages/home/auth/sign_in_page.dart';
 import 'package:expedia/pages/home/cars/cars_page.dart';
 import 'package:expedia/pages/home/flights/flights_home.dart';
 import 'package:expedia/pages/home/package/packages_page.dart';
 import 'package:expedia/pages/home/stays/stays_home.dart';
 import 'package:flutter/material.dart';
 import 'widgets/top_tab_bar.dart';
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -29,21 +29,21 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: const Color.fromRGBO(40, 247, 178, 1),
+        backgroundColor: Colors.transparent,
         title: Row(
           children: [
-            // Image.asset("assets/images/expedia_logo.png", height: 40),
-            Expanded(child: Text("Flight App")),
+            Image.asset("assets/images/expedia_logo.png", height: 40),
+            // Expanded(child: Text("Flight App")),
           ],
         ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Get.to(() => const SignInPage());
-            },
-            child: Text("Sign In"),
-          ),
-        ],
+        // actions: [
+        //   TextButton(
+        //     onPressed: () {
+        //       Get.to(() => const SignInPage());
+        //     },
+        //     child: Text("Sign In"),
+        //   ),
+        // ],
       ),
       body: Column(
         children: [
