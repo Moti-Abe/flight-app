c2638fa04d6bbf10b39aae8b1f2ba2197063591f
Tuamay <tuamayassefa@gmail.com>
2026-02-16 22:09:43 -0800

    one way widget

---
+++ lib/pages/search/search_flight_page.dart
@@ -0,0 +1,46 @@
+import 'package:expedia/pages/home/flights/multi_city_page.dart';
+import 'package:flutter/material.dart';
+import '../home/flights/widgets/flight_tab_bar.dart';
+import 'package:expedia/pages/home/flights/OnewayPage/one_way_page.dart';
+import 'package:expedia/pages/home/flights/round_trip.dart';
+
+class SearchFlightPage extends StatefulWidget {
+  const SearchFlightPage({super.key});
+
+  @override
+  State<SearchFlightPage> createState() => _SearchFlightPageState();
+}
+
+class _SearchFlightPageState extends State<SearchFlightPage> {
+  int _flightIndex = 0;
+
+  final List<Widget> _flightPages = const [
+    RoundtripPage(),
+    OneWayPage(),
+    MultiCityPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Search Flights")),
+      body: SafeArea(
+        child: Column(
+          children: [
+            FlightTabBar(
+              selectedIndex: _flightIndex,
+              onChanged: (index) {
+                setState(() {
+                  _flightIndex = index;
+                });
+              },
+            ),
+            const Divider(height: 1),
+
+            Expanded(child: _flightPages[_flightIndex]),
+          ],
+        ),
+      ),
+    );
+  }
+}
