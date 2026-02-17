46b8aa740271daed0664339d2d05f5bcf63442c7
Tuamay <tuamayassefa@gmail.com>
2026-02-17 10:13:01 -0800

    modified widgets

--- lib/pages/search/search_flight_page.dart
+++ lib/pages/search/search_flight_page.dart
@@ -2,7 +2,7 @@ import 'package:expedia/pages/home/flights/multi_city_page.dart';
 import 'package:flutter/material.dart';
 import '../home/flights/widgets/flight_tab_bar.dart';
 import 'package:expedia/pages/home/flights/OnewayPage/one_way_page.dart';
-import 'package:expedia/pages/home/flights/round_trip.dart';
+// import 'package:expedia/pages/home/flights/round_trip.dart';
 
 class SearchFlightPage extends StatefulWidget {
   const SearchFlightPage({super.key});
@@ -15,7 +15,7 @@ class _SearchFlightPageState extends State<SearchFlightPage> {
   int _flightIndex = 0;
 
   final List<Widget> _flightPages = const [
-    RoundtripPage(),
+    // RoundtripPage(),
     OneWayPage(),
     MultiCityPage(),
   ];
